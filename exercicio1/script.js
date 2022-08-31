e9dccbe3b8bad2d5dd86cfbcdd19eae119ceffb7
ludmilajf <ludmiladjf@gmail.com>
2022-08-31 13:45:11 -0300

    Exerício 1 - concluído

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+const nome = prompt('Digite seu nome')
+const comida1 = prompt('Digite sua comida preferida (1/3')
+const comida2 = prompt('Digite sua comida preferida (2/3')
+const comida3 = prompt('Digite sua comida preferida (3/3')
+
+console.log(`
+Estas são as comidas favoritas de ${nome}:
+- ${comida1};
+- ${comida2};
+- ${comida3}.
+`)
